ddbc2c8c686398201e162987883bfd695d12e6a2
amanyashc2003 <168587538+amanyashc2003@users.noreply.github.com>
2024-05-08 02:06:40 +0530

    Add files via upload

---
+++ Aman.java
@@ -0,0 +1,14 @@
+
+public class Aman {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=12;
+		int  b=23;
+		int c=a+b;
+		System.out.println("sum of two number"+c);
+                  
+	}
+
+}
